15a09b44a09021718a6f3d937cbbdb0031e43cc9
Prthmsh7 <Prathmeshshukla2106@gmail.com>
2025-03-10 08:35:04 -0700

    updated the project

--- client/src/index.js
+++ client/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import { CssBaseline, ThemeProvider, createTheme } from '@mui/material';
 
+
 const theme = createTheme({
   palette: {
     mode: 'dark',
